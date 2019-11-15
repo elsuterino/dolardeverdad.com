4583c8c4011b0189658bb21d05873881d665907e
tecnologioVE <albertvivas@gmail.com>
2019-11-15 09:14:43 -0400

    a
    a

--- public/js/app.js
+++ public/js/app.js
@@ -105165,8 +105165,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /usr/share/nginx/dolardeverdad.com/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /usr/share/nginx/dolardeverdad.com/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\dolardeverdad\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\dolardeverdad\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,10 @@
     <!-- Fonts -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -38,91 +41,148 @@
         gtag('config', 'UA-40405144-9');
     </script>
 </head>
-<body class="open-sans">
-<div class="min-h-screen bg-blue-500 bg">
-    <section class="container mx-auto pt-12 md:pt-32 p-4">
-
-        <p class="pb-4 text-5xl font-bold text-white roboto">
-            Dolar de verdad
-        </p>
+<body class="open-sans text-xl bg-blue-800 bg">
+    <section class="container mx-auto pt-12 md:pt-32 p-2">
+        <div class="pb-4">
+            <p class="text-5xl font-bold text-white">
+                Dolar de verdad
+            </p>                
+        </div>
         <div id="app" class="md:flex">
-            <div class="md:w-1/2">
-                <div class="pb-4 text-blue-lighter">
-                    <p class="pb-4 leading-normal">
-                        Dile adiós a todos los promedios, a los animales raros y a las casas de cambio que nadie sabe
-                        dónde quedan, la tasa actual es:
-                    </p>
+            <div class="md:w-1/2 px-1">
+                <div>
+                    <div class="pb-4">
+                        <p class="text-blue-lighter">
+                                    Dile adiós a todos los promedios, a los animales raros y a las casas de cambio que nadie sabe
+                                    dónde quedan, la tasa actual es:
+                        </p>
+                    </div>
+                    <div class="flex-col rounded-lg xl:mr-2">
+                            <div class="flex justify-around">
+                                <div class="border border-blue-600 rounded-lg p-2 m-1">
+                                <p class="text-2xl text-blue-light font-bold roboto">
+                                        1
+                                        <sub class="text-xl">EUR</sub> = {{ number_format(Cache::get('latest:ves-local-coinbase')) }}
+                                        <sub class="text-xl">VES</sub>
+                                    </p>
+                                </div>
 
-                    <p class="pb-4 text-5xl text-blue-light font-bold roboto">
-                        1<sub class="text-2xl">USD</sub> = {{ number_format(Cache::get('latest:ves-local-coinbase')) }}
-                        <sub
-                            class="text-2xl">VES</sub>
-                    </p>
+                                <div class="border border-blue-600 rounded-lg p-2 m-1">
+                                    <p class="text-2xl text-blue-light font-bold roboto">
+                                        1
+                                        <sub class="text-lg">USD</sub> = {{ number_format(Cache::get('latest:ves-local-coinbase')) }}
+                                        <sub class="text-lg">VES</sub>
+                                    </p>
+                                </div>
 
-                    <p class="pb-4  leading-normal">
-                        ¿De dónde se obtiene el precio? de la simple oferta y demanda, es decir, de las transacciones
-                        reales completadas del sitio
-                        <a href="https://localbitcoins.com/es/?ch=ykb7" class="text-blue hover:text-blue-lighter">
-                            localbitcoins.com
-                        </a>
-                    </p>
+                            </div>
+                            <div class="flex justify-around">
+                                <div class="border border-blue-600 rounded-lg p-2 m-1">
+                                    <p class="text-2xl text-blue-light font-bold roboto">
+                                        1
+                                        <sub class="text-lg">ARS</sub> = {{ number_format(Cache::get('latest:ves-local-coinbase')) }}
+                                        <sub class="text-lg">VES</sub>
+                                    </p>
+                                </div>
+                                <div class="border border-blue-600 rounded-lg p-2 m-1">
+                                    <p class="text-2xl text-blue-light font-bold roboto">
+                                        1
+                                        <sub class="text-lg">CLP</sub> = {{ number_format(Cache::get('latest:ves-local-coinbase')) }}
+                                        <sub class="text-lg">VES</sub>
+                                    </p>
+                                </div>       
+                                <div class="border border-blue-600 rounded-lg p-2 m-1">
+                                    <p class="text-2xl text-blue-light font-bold roboto">
+                                        1
+                                        <sub class="text-lg">COP</sub> = {{ number_format(Cache::get('latest:ves-local-coinbase')) }}
+                                        <sub class="text-lg">VES</sub>
+                                    </p>
+                                </div>
+                                <div class="border border-blue-600 rounded-lg p-2 m-1">
+                                    <p class="text-2xl text-blue-light font-bold roboto">
+                                        1
+                                        <sub class="text-lg">PEN</sub> = {{ number_format(Cache::get('latest:ves-local-coinbase')) }}
+                                        <sub class="text-lg">VES</sub>
+                                    </p>
+                                </div>                                                          
+                            </div>
+                    </div>
+                    <div class="pb-4 text-blue-lighter">
+                        <p class="pb-4  leading-normal">
+                            ¿De dónde se obtiene el precio? de la simple oferta y demanda, es decir, de las transacciones
+                            reales completadas del sitio
+                            <a href="https://localbitcoins.com/es/?ch=ykb7" class="text-blue hover:text-blue-lighter">
+                                localbitcoins.com
+                            </a>
+                        </p>
 
-                    <p class="pb-4">
-                        Durante las últimas 24 horas se han completado transacciones de compra/venta por un costo total
-                        de:
-                    </p>
+                        <p class="pb-4">
+                            Durante las últimas 24 horas se han completado transacciones de compra/venta por un costo total
+                            de:
+                        </p>
 
-                    <p class="pb-4">
-                        <span
-                            class="text-blue-light text-lg">{{ number_format(Cache::get('latest:localhost:moved-today:ves')) }}</span>
-                        Bolivares
-                    </p>
+                        <p class="pb-4">
+                            <span
+                                class="text-blue-light text-lg">{{ number_format(Cache::get('latest:localhost:moved-today:ves')) }}</span>
+                            Bolivares
+                        </p>
 
-                    <p class="pb-4">
-                        <span
-                            class="text-blue-light text-lg">{{ number_format(Cache::get('latest:localhost:moved-today:usd')) }}</span>
-                        Dolares
-                    </p>
+                        <p class="pb-4">
+                            <span
+                                class="text-blue-light text-lg">{{ number_format(Cache::get('latest:localhost:moved-today:usd')) }}</span>
+                            Dolares
+                        </p>
+                    </div>
+					
                 </div>
-
             </div>
-
-            <div class="md:w-1/2">
-                <v-chart :data='{!! json_encode(Cache::get('chart-dataset')) !!}'></v-chart>
+            <div class="md:w-1/2 px-1">
+                    chart
+                    <v-chart :data='{!! json_encode(Cache::get('chart-dataset')) !!}'></v-chart>
             </div>
         </div>
         <div id="ad">
-
-            <script type="text/javascript">
-                amzn_assoc_ad_type = "banner";
-                amzn_assoc_marketplace = "amazon";
-                amzn_assoc_region = "US";
-                amzn_assoc_placement = "assoc_banner_placement_default";
-                amzn_assoc_campaigns = "wireless";
-                amzn_assoc_banner_type = "category";
-                amzn_assoc_p = "48";
-                amzn_assoc_isresponsive = "false";
-                amzn_assoc_banner_id = "0TJY74H4VE9W4MDG6J82";
-                amzn_assoc_width = "728";
-                amzn_assoc_height = "90";
-                amzn_assoc_tracking_id = "inetvi0c-20";
-                amzn_assoc_linkid = "9d18c3020e78550dd6753f37b13e628d";
-            </script>
-            <script src="//z-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1"></script>
-            <div id="subad" class="flex justify-center text-white text-sm mt-1">
-                <div class="inline-flex">
-                    <img src="https://brave.com/wp-content/uploads/2018/02/brave_icon_512x_twitter.png" class="h-4 w-4">
-                    <a href="https://brave.com/dol014" class="mr-1 text-orange-light hover:text-orange">Brave</a>
-                    is a browser that is build on chrome, with attention to privacy, speed and blocking ads like theese.
+        <script type="text/javascript">
+                    amzn_assoc_ad_type = "banner";
+                    amzn_assoc_marketplace = "amazon";
+                    amzn_assoc_region = "US";
+                    amzn_assoc_placement = "assoc_banner_placement_default";
+                    amzn_assoc_campaigns = "wireless";
+                    amzn_assoc_banner_type = "category";
+                    amzn_assoc_p = "48";
+                    amzn_assoc_isresponsive = "false";
+                    amzn_assoc_banner_id = "0TJY74H4VE9W4MDG6J82";
+                    amzn_assoc_width = "728";
+                    amzn_assoc_height = "90";
+                    amzn_assoc_tracking_id = "inetvi0c-20";
+                    amzn_assoc_linkid = "9d18c3020e78550dd6753f37b13e628d";
+                </script>
+                <script src="//z-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1"></script>
+                <div id="subad" class="flex justify-center text-white text-sm mt-1">
+                    <div class="inline-flex">
+                        <img src="https://brave.com/wp-content/uploads/2018/02/brave_icon_512x_twitter.png" class="h-4 w-4">
+                        <a href="https://brave.com/dol014" class="mr-1 text-orange-light hover:text-orange">Brave</a>
+                        is a browser that is build on chrome, with attention to privacy, speed and blocking ads like theese.
+                    </div>
                 </div>
-            </div>
         </div>
-
     </section>
-</div>
-<script src="{{ asset('js/app.js') }}"></script>
-<script>
+    
 
-</script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
+
+
+
+
+  
+ 
+
+    
+
+                
+
+
+                
+
