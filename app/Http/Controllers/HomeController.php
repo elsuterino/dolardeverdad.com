fe8dbbc340a1db1f1e3fd83bb488e1e1b6bc30a2
Vyatuatas Riauka <vytautas.riauka@mtcmedia.co.uk>
2019-11-16 11:30:58 +0200

    fixed to seo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,16 +12,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        JsonLd::addValue("mainEntity", [
-            "@type" => "ExchangeRateSpecification",
-            "currency" => "USD",
-            "currentExchangeRate" => [
-                "@type" => "UnitPriceSpecification",
-                "price" => round(getCurrency('VESUSD', 'value'), 2),
-                "priceCurrency" => "VES",
-            ],
-        ]);
+//        JsonLd::addValue("mainEntity", [
+//            "@type" => "ExchangeRateSpecification",
+//            "currency" => "USD",
+//            "currentExchangeRate" => [
+//                "@type" => "UnitPriceSpecification",
+//                "price" => round(getCurrency('VESUSD', 'value'), 2),
+//                "priceCurrency" => "VES",
+//            ],
+//        ]);
 
+        $currenciesToCompare = ['USD', 'EUR', 'COP', 'ARS', 'CLP', 'PEN'];
+        JsonLd::addValue("mainEntity", [
+            "@type" => 'ItemList',
+            "ItemListElement" =>
+                array_map(function ($currency) {
+                    return [
+                        "@type" => "ExchangeRateSpecification",
+                        "currency" => $currency,
+                        "currentExchangeRate" => [
+                            "@type" => "UnitPriceSpecification",
+                            "price" => round(getCurrency("VES{$currency}", 'value'), 2),
+                            "priceCurrency" => "VES",
+                        ],
+                    ];
+                }, ['USD', 'EUR', 'COP', 'ARS', 'CLP', 'PEN']),
+        ]);
         return view('home');
     }
 }
